f8162514e93eceadbca36b1f9dc526719bae5eb1
nekusaetsa <nekusaetsa@yandex.ru>
2024-08-03 13:04:25 +0300

    first steps

---
+++ how-are-you-in-fantasy-world/Scenes/main.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var q1 = "QUES_1"
+var q2 = "QUES_2"
+var list = ["q1","q2","q3","q4"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func skip():
+	$Label.text = tr(q2)
+	print(list[2])
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
